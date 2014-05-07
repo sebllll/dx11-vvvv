5e739950106adf00d61b3af54d742e8a3c4612bd
sebl <sebl@schnellebuntebilder.de>
2014-05-08 01:29:48 +0200

    init - untested

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/AsRaw.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using SlimDX.Direct3D11;
+
+using VVVV.Core.Logging;
+
+using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+
+using System.IO;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "AsRaw", Category = "DX11.Texture", Version = "2d", Author = "vux", AutoEvaluate = true)]
+    public class AsRawTextureNode : IPluginEvaluate, IDX11ResourceDataRetriever
+    {
+        [Input("Texture In")]
+        protected Pin<DX11Resource<DX11Texture2D>> FTextureIn;
+
+        [Input("Filename", StringType = StringType.Filename, DefaultString = "render")]
+        protected ISpread<string> FInPath;
+
+        [Input("Format")]
+        protected ISpread<ImageFileFormat> FInFormat;
+
+        [Input("Write", IsBang = true)]
+        protected ISpread<bool> FInSave;
+
+        //[Output("Output")]
+        //protected ISpread<string> FOutputd;
+
+        [Output("Output")]
+        public ISpread<Stream> FStreamOut;
+
+        [Output("Valid")]
+        protected ISpread<bool> FOutValid;
+
+        [Import()]
+        protected IPluginHost FHost;
+
+        [Import()]
+        protected ILogger FLogger;
+
+        public DX11RenderContext AssignedContext
+        {
+            get;
+            set;
+        }
+
+        public event DX11RenderRequestDelegate RenderRequest;
+
+
+        #region IPluginEvaluate Members
+
+        public void Evaluate(int SpreadMax)
+        {
+            this.FOutValid.SliceCount = 1;
+
+            if (this.FTextureIn.PluginIO.IsConnected)
+            {
+                if (this.RenderRequest != null) { this.RenderRequest(this, this.FHost); }
+
+                if (this.AssignedContext == null) { this.FOutValid.SliceCount = 0; return; }
+                //Do NOT cache this, assignment done by the host
+
+                for (int i = 0; i < SpreadMax; i++)
+                {
+                    if (this.FTextureIn[i].Contains(this.AssignedContext) && this.FInSave[i])
+                    {
+                        try
+                        {
+                            Texture2D.ToStream(this.AssignedContext.CurrentDeviceContext, this.FTextureIn[i][this.AssignedContext].Resource, this.FInFormat[i], FStreamOut[i]);
+                            //Texture2D.SaveTextureToFile(this.AssignedContext.CurrentDeviceContext, this.FTextureIn[i][this.AssignedContext].Resource, this.FInFormat[i], this.FInPath[i]);
+                            this.FOutValid[0] = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            FLogger.Log(ex);
+                            this.FOutValid[0] = false;
+                        }
+                    }
+                    else
+                    {
+                        this.FOutValid[0] = false;
+                    }
+                }
+            }
+            else
+            {
+                this.FOutValid.SliceCount = 0;
+
+            }
+        }
+
+        #endregion
+    }
+}
