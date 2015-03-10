f5df48af549bd216be41c8c9e2fb1a2924011545
sebl <sebl@schnellebuntebilder.de>
2015-03-10 18:53:53 +0100

    duplicated again - this time it should have worked

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Renderers/Graphics/OVRNode_ctrl.Designer.cs
@@ -0,0 +1,26 @@
+﻿namespace VVVV.DX11.Nodes
+{
+    partial class OVRNode
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            this.FOutBackBuffer[0].Dispose();
+            this.depthmanager.Dispose();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
