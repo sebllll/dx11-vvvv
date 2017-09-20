f7667ec8da87b9e56dbc58c0da8783aa3ac93987
sebl <sebl@schnellebuntebilder.de>
2017-09-20 17:38:40 +0200

    adds PointerNodes

---
+++ PointerNodes/FromPointer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.PluginInterfaces.V1;
+using SlimDX.Direct3D11;
+
+
+namespace VVVV.DX11.Nodes.Textures
+{
+    public class DX11Texture2D_extension : DX11Texture2D
+    {
+        public static DX11Texture2D_extension FromPointer(DX11RenderContext context, IntPtr pointer)
+        {
+            Texture2D tex = Texture2D.FromPointer(pointer);
+            ShaderResourceView srv = new ShaderResourceView(context.Device, tex);
+
+            DX11Texture2D_extension result = new DX11Texture2D_extension();
+
+            result.context = context;
+            result.Resource = tex;
+            result.SRV = srv;
+            result.desc = tex.Description;
+
+            result.isowner = false;
+            return result;
+        }
+    }
+
+    public class DX11Texture3D_extension : DX11Texture3D
+    {
+        public static DX11Texture3D FromPointer(DX11RenderContext context, IntPtr pointer)
+        {
+            Texture3D tex = Texture3D.FromPointer(pointer);
+            ShaderResourceView srv = new ShaderResourceView(context.Device, tex);
+
+            DX11Texture3D_extension result = new DX11Texture3D_extension();
+            result.context = context;
+            result.Resource = tex;
+            result.SRV = srv;
+            result.desc = tex.Description;
+
+            result.isowner = false;
+            return result;
+        }
+    }
+
+
+    [PluginInfo(Name = "FromPointer", Category = "DX11.Texture", Version = "2d", Author = "vux")]
+    public class FromPointer2DNode : FromPointerGenericNode<DX11Texture2D>
+    {
+        public override DX11Texture2D newTexture(DX11RenderContext context, IntPtr handle)
+        {
+            return DX11Texture2D_extension.FromPointer(context, handle);
+        }
+    }
+
+    [PluginInfo(Name = "FromPointer", Category = "DX11.Texture", Version = "3d", Author = "vux")]
+    public class FromPointer3DNode : FromPointerGenericNode<DX11Texture3D>
+    {
+        public override DX11Texture3D newTexture(DX11RenderContext context, IntPtr handle)
+        {
+            return DX11Texture3D_extension.FromPointer(context, handle);
+        }
+    }
+
+    public abstract class FromPointerGenericNode<T> : IPluginEvaluate, IDX11ResourceHost, IDisposable where T : IDX11Resource
+    {
+        [Input("Pointer", AsInt = true)]
+        protected IDiffSpread<long> FPointer;
+
+        [Output("Texture")]
+        protected Pin<DX11Resource<T>> FTextureOutput;
+
+        [Output("Is Valid")]
+        protected ISpread<bool> FValid;
+
+        protected bool FInvalidate;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FPointer.SliceCount == 0)
+            {
+                this.FTextureOutput.SafeDisposeAll();
+                this.FTextureOutput.SliceCount = 0;
+
+                return;
+            }
+
+            this.FValid.SliceCount = SpreadMax;
+            this.FTextureOutput.SliceCount = SpreadMax;
+
+
+            if (this.FPointer.IsChanged)
+            {
+                this.FInvalidate = true;
+                this.FTextureOutput.SafeDisposeAll();
+            }
+
+            for (int i = 0; i < SpreadMax; i++)
+            {
+                if (this.FTextureOutput[i] == null)
+                {
+                    FTextureOutput[i] = new DX11Resource<T>();
+                }
+            }
+
+        }
+
+        public void Update(DX11RenderContext context)
+        {
+            if (this.FInvalidate)
+            {
+                for (int i = 0; i < FTextureOutput.SliceCount; i++)
+                {
+                    try
+                    {
+                        IntPtr share = new IntPtr(FPointer[i]);
+
+                        this.FTextureOutput[i][context] = newTexture(context, share);
+                        this.FValid[i] = true;
+
+                    }
+                    catch (Exception)
+                    {
+                        this.FValid[i] = false;
+                    }
+                }
+                this.FInvalidate = false;
+            }
+        }
+
+        public abstract T newTexture(DX11RenderContext context, IntPtr handle);
+
+
+        public void Destroy(DX11RenderContext context, bool force)
+        {
+            this.FTextureOutput.SafeDisposeAll(context);
+        }
+
+        public void Dispose()
+        {
+            this.FTextureOutput.SafeDisposeAll();
+        }
+    }
+}
---
+++ PointerNodes/ToPointer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.PluginInterfaces.V1;
+
+using SlimDX.Direct3D11;
+using System.ComponentModel.Composition;
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+namespace VVVV.DX11.Nodes.Textures
+{
+   
+    [PluginInfo(Name = "ToPointer", Category = "DX11.Texture", Version = "2d", Author = "vux,tonfilm")]
+    public class ToPointer2DNode : ToPointerGenericNode<DX11Texture2D>
+    {
+        public override long calcPointer(int i, DX11RenderContext context)
+        {
+            return this.FTextureIn[i][context].Resource.ComPointer.ToInt64();
+        }
+    }
+
+    //[PluginInfo(Name = "ToPointer", Category = "DX11.Texture", Version = "Cube", Author = "vux,tonfilm")]
+    //public class ToPointerCubeNode : ToPointerGenericNode<DX11CubeRenderTarget>
+    //{
+    //    public override long calcPointer(int i, DX11RenderContext context)
+    //    {
+    //        return this.FTextureIn[i][context].Resource.ComPointer.ToInt64();
+    //    }
+    //}
+
+    [PluginInfo(Name = "ToPointer", Category = "DX11.Texture", Version = "3d", Author = "vux,tonfilm")]
+    public class ToPointer3DNode : ToPointerGenericNode<DX11Texture3D>
+    {
+        public override long calcPointer(int i, DX11RenderContext context)
+        {
+            return this.FTextureIn[i][context].Resource.ComPointer.ToInt64();
+        }
+    }
+
+    public abstract class ToPointerGenericNode<T> : IPluginEvaluate, IDX11ResourceDataRetriever, IDisposable where T : IDX11Resource
+    {
+        [Import()]
+        protected IPluginHost FHost;
+
+        [Input("Texture In")]
+        protected Pin<DX11Resource<T>> FTextureIn;
+
+        [Output("Pointer", AsInt = true)]
+        protected ISpread<long> FPointer;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FTextureIn.PluginIO.IsConnected)
+            {
+
+                if (this.RenderRequest != null) { this.RenderRequest(this, this.FHost); }
+
+                if (this.AssignedContext == null)
+                {
+                    return;
+                }
+
+                this.FPointer.SliceCount = SpreadMax;
+
+                DX11RenderContext context = this.AssignedContext;
+
+
+
+                for (int i = 0; i < SpreadMax; i++)
+                {
+                    if (this.FTextureIn[0].Contains(context))
+                    {
+                        FPointer[i] = calcPointer(i, context);
+                    }
+                    else
+                    {
+                        FPointer[i] = -1;
+                    }
+                }
+
+            }
+            else
+            {
+
+            }
+        }
+
+        public abstract Int64 calcPointer(int i, DX11RenderContext context);
+
+
+        private void SetDefault(int i)
+        {
+            FPointer[i] = -1;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public DX11RenderContext AssignedContext
+        {
+            get;
+            set;
+        }
+
+        public event DX11RenderRequestDelegate RenderRequest;
+    }
+    
+}
